09f924de0e7ee55e62dc6f4b49070712b1b7bea1
Alex Chojnowski <achojnowski00@gmail.com>
2022-06-05 14:10:31 +0200

    regresja liniowa

--- regresjaLiniowa.py
+++ regresjaLiniowa.py
@@ -2,25 +2,25 @@ import math as m
 import numpy as np
 
 
-def lewaOdwrotnosc(macierz):
-    mDotMT = np.dot(macierz.T, macierz)
-    macierzOdwrotna = np.linalg.inv(mDotMT)
-    return np.dot(macierzOdwrotna, macierz.T)
-
-
 def regresjaLiniowa(matrix):
-    matrix_x = np.array([[1, x[0]]for x in matrix])
-    matrix_y = np.array([x[1]for x in matrix])
-    lewa_odw = lewaOdwrotnosc(matrix_x)
-    return np.dot(lewa_odw, matrix_y)
+    macierzX = np.array([[1, x[0]] for x in matrix])
+    macierzY = np.array([y[1] for y in matrix])
+    MT_dot_M_to_power_minus_one = np.linalg.inv(np.dot(macierzX.T, macierzX))
+    lewa_odwrotnosc = np.dot(MT_dot_M_to_power_minus_one, macierzX.T)
+    macierzBeta = np.dot(lewa_odwrotnosc, macierzY)
+    return macierzBeta 
 
 
-matriz = np.array([[2, 1],
-                   [5, 2],
-                   [7, 3],
-                   [8, 3]])
+matrixZPunktow = np.array([
+    [2, 1],
+    [5, 2],
+    [7, 3],
+    [8, 3]
+])
 
-print(regresjaLiniowa(matriz))
+beta = regresjaLiniowa(matrixZPunktow)
+print(beta)
+print("f(x) = ", beta[1], "x + ", beta[0],sep="")
 
 
 '''
@@ -31,8 +31,8 @@ punkty do zadania
 (8, 3)
 
 Beta:
-[2 / 7 ]
-[5 / 14]
+[2 / 7 ] 0.28571429
+[5 / 14] 0.35714286
 
 wyklad 4 kwietnia 2022 
 17 minuta omówienie tego zadania
